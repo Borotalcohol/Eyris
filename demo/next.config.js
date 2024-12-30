4973ce1dc854fecd045f361d504acc6222516099
borotalcohol <loschiavo.christian@gmail.com>
2024-12-30 21:03:09 +0100

    Updated next config

--- demo/next.config.js
+++ demo/next.config.js
@@ -2,15 +2,18 @@
 const path = require("path"); // Import the path module
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+// import TerserPlugin from "terser-webpack-plugin";
 
 const nextConfig = {
   // output: "export",
   reactStrictMode: true,
+  // swcMinify: false,
   //distDir: 'build',
   // images: {
   //   unoptimized: true,
   // },
-  webpack: (config, {}) => {
+  webpack: (config) => {
     config.resolve.extensions.push(".ts", ".tsx");
     config.resolve.fallback = { fs: false };
 
@@ -18,6 +21,18 @@ const nextConfig = {
       ...config.resolve.alias,
       "@": path.resolve(__dirname, "./"),
     };
+
+    config.optimization.minimizer = [
+      new TerserPlugin({
+        exclude: /node_modules\/jimp/,
+        terserOptions: {
+          mangle: true,
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+    ];
 
     config.plugins.push(
       new NodePolyfillPlugin(),
@@ -31,14 +46,6 @@ const nextConfig = {
             from: "./node_modules/onnxruntime-web/dist/ort-wasm-simd-threaded.wasm",
             to: "static/chunks/pages",
           },
-          // {
-          //   from: "./node_modules/onnxruntime-web/dist/ort-wasm.wasm",
-          //   to: "static/chunks/pages",
-          // },
-          // {
-          //   from: "./node_modules/onnxruntime-web/dist/ort-wasm-simd.wasm",
-          //   to: "static/chunks/pages",
-          // },
           {
             from: "./model",
             to: "static/chunks/pages",
